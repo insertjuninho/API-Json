06b590a349c11088d53a82d57f0dfd129bb81a26
insertjuninho <juniorsax3@gmail.com>
2018-10-26 12:55:15 -0300

    Atualizacao

--- src/pages/telefone/telefone.ts
+++ src/pages/telefone/telefone.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { TelefoneService } from '../../service/telefone.service';
 import { Telefone } from '../../model/telefone';
 
@@ -11,16 +11,33 @@ import { Telefone } from '../../model/telefone';
 export class TelefonePage {
 
   telefones : Telefone[];
-
+  telefonePage: Telefone[] = [];
+  page : number = 0;
+  
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              public service : TelefoneService) {
+              public service : TelefoneService,
+              public loading : LoadingController) {
 
   }
 
   ionViewDidLoad() {
+   this.getTelefone();
+  }
+
+  getTelefone(){
     this.service.getTelefone()
     .subscribe(response => {
       this.telefones = response;
+      this.addPage();
     });
+
   }
+  addPage(){
+    for(var i = 0; i<10; i++){
+      this.telefonePage.push(this.telefones[this.page]);
+      this.page++;
+    }
+    console.log(this.telefonePage);
+  }
+
 }
